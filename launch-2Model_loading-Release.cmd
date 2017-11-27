199ca5269ce10a89fe6f3dfd0c0112c74f5ec398
A-Youssry <ahmedyoussry1996@gmail.com>
2017-11-27 18:52:58 +0200

    Add project files.

---
+++ launch-2Model_loading-Release.cmd
@@ -0,0 +1,9 @@
+cd C:/Users/ahmed/Desktop/Graphics Lab 4-20171125T142711Z-001/Graphics Lab 4/Lab4/2Model_loading/
+set PATH=;%PATH%
+
+if [%1]==[--debugger] (
+	SHIFT
+)
+
+"C:\Users\ahmed\Desktop\Graphics Lab 4-20171125T142711Z-001\Graphics Lab 4\Lab4\Build\Release\2Model_loading.exe"  
+pause
---
+++ launch-3Lighting-Debug.cmd
@@ -0,0 +1,9 @@
+cd C:/Users/ahmed/Desktop/Graphics Lab 4-20171125T142711Z-001/Graphics Lab 4/Lab4/3Lighting/
+set PATH=;%PATH%
+
+if [%1]==[--debugger] (
+	SHIFT
+)
+
+"C:\Users\ahmed\Desktop\Graphics Lab 4-20171125T142711Z-001\Graphics Lab 4\Lab4\Build\Debug\3Lighting.exe"  
+pause
